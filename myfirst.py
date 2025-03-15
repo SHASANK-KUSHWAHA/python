f7ce65fe72b9e8c16971201456f00fe7d412ed28
SHASANK-KUSHWAHA <shasankkushwaha2006@gmail.com>
2025-03-15 21:49:35 +0530

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,24 @@
+#today we are going to code object orinented programming in python
+class animal:
+    def __init__(self,name,age):
+      self.name = name
+      self.age =age
+    def bark(self):
+      return f"{self.name} says woof!"
+      x=53
+      print(x)
+my = animal("tom",52)
+print(my.bark())
+ 
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        return f"{self.name} says woof!"
+
+# Creating an object of the Dog class
+my_dog = Dog("Buddy", 3)
+print(my_dog.bark())  # Output: Buddy says woof!
